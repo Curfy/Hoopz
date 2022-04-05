429b01a5a06054c5f1a75328a50b908187f5334d
AApalafox <adreanpalafox@gmail.com>
2022-04-05 13:50:53 +0800

    added matches page and some page elements

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,12 @@
 <?php
-  if(!isset($_COOKIE['isLogin'])){
+if (!isset($_COOKIE['isLogin'])) {
     header("location: ../login.php");
-  }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,17 +16,19 @@
     <!-- MATERIAL ICONS -->
     <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
     <!-- STYLESHEET -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
+
 <body>
     <div class="container">
         <aside>
             <div class="top">
                 <div class="logo">
                     <img src="./images/hoopzlogo.png" alt="">
-                    <h2 class = "danger">HO<span class="score">OPZ</span></h2>
+                    <h2 class="danger">HO<span class="score">OPZ</span></h2>
                 </div>
                 <div class="close" id="close-btn">
                     <span class="material-icons-sharp">close</span>
@@ -39,7 +42,7 @@
                     <h3>Dashboard</h3>
                 </a>
 
-                <a href="#" class = "active">
+                <a href="#" class="active">
                     <span class="material-icons-outlined">sports_esports</span>
                     <h3>Matches</h3>
                 </a>
@@ -57,9 +60,9 @@
                 <a href="#">
                     <span class="material-icons-outlined">mail_outline</span>
                     <h3>Messages</h3>
-                    <span class = "message-count">26</span>
+                    <span class="message-count">26</span>
                 </a>
-                
+
                 <a href="#">
                     <span class="material-icons-outlined">trending_up</span>
                     <h3>Analytics</h3>
@@ -70,7 +73,7 @@
                     <h3>Settings</h3>
                 </a>
 
-                <a id = "logoutBtn" class = "logout">
+                <a id="logoutBtn" class="logout">
                     <span class="material-icons-outlined">logout</span>
                     <h3>Logout</h3>
                 </a>
@@ -86,18 +89,18 @@
                     <input type="text" placeholder="Search Player">
                 </div> -->
                 <div class="top">
-                    <button id = "menu-btn">
+                    <button id="menu-btn">
                         <span class="material-icons-round">menu</span>
                     </button>
                     <div class="theme-toggler">
                         <span class="material-icons-round">light_mode</span>
                         <span class="material-icons-round">dark_mode</span>
                     </div>
-    
+
                     <div class="profile">
                         <div class="info">
                             <p>Welcome, <b>Zenovain</b></p>
-                            <small class ="text-muted"><b class = "success">Head of Staff</b></small>
+                            <small class="text-muted"><b class="success">Head of Staff</b></small>
                         </div>
                         <div class="profile-photo">
                             <img src="./images/zenovain.jpg" alt="">
@@ -106,11 +109,18 @@
                 </div>
             </div>
 
+            <!-- page content here -->
+            <div class="content">
+                <?php include 'matches.php' ?>
+            </div>
+
         </main>
         <!-- END OF MAIN -->
     </div>
 
-    <script src = "./index.js"></script>
-    <script src = "../js/main.js"></script>
+    <script src="./index.js"></script>
+    <script src="../js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
---
+++ pages/matches.php
@@ -0,0 +1,89 @@
+<!-- CARD DISPLAY TEMPLATE -->
+<div class="row gx-1">
+	<!-- LEFT -->
+	<div class="col-lg-5 text-center">
+		<div class="card shadow-sm">
+			<div class="card-body">
+				<div class="row justify-content-center g-2">
+					<div class="col-sm">
+						<div class="card-body">
+							test
+						</div>
+					</div>
+					<div class="col-sm">
+						<div class="card-body">
+							test
+						</div>
+					</div>
+					<div class="col-sm">
+						<div class="card-body">
+							test
+						</div>
+					</div>
+					<div class="col-sm">
+						<div class="card-body">
+							test
+						</div>
+					</div>
+					<div class="col-sm">
+						<div class="card-body">
+							test
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card">
+				<div class="row g-0">
+					<div class="col-sm">
+						<div class="foot-btn">
+							test
+						</div>
+					</div>
+					<div class="col-sm">
+						<div class="foot-btn foot-btn-center">
+							test
+						</div>
+					</div>
+					<div class="col-sm">
+						<div class="foot-btn">
+							test
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- RIGHT -->
+	<div class="col-lg">
+		<div class="card shadow-sm">
+			<div class="card-body">
+				<p>Game Leaders</p>
+				<table class="table">
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">First</th>
+							<th scope="col">Last</th>
+							<th scope="col">Handle</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Mark</td>
+							<td>Otto</td>
+							<td>@mdo</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td>Jacob</td>
+							<td>Thornton</td>
+							<td>@fat</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END CARD DISPLAY TEMPLATE-->
