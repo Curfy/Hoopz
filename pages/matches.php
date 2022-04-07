2eebccc15b1590277fa33a02050c9075e8947af5
AApalafox <adreanpalafox@gmail.com>
2022-04-07 18:52:27 +0800

    added static elements and design

--- pages/matches.php
+++ pages/matches.php
@@ -1,33 +1,43 @@
 <!-- CARD DISPLAY TEMPLATE -->
-<div class="row gx-1">
+<div class="row gx-1 mb-3">
 	<!-- LEFT -->
-	<div class="col-lg-5 text-center">
-		<div class="card shadow-sm">
+	<div class="col-xl-5 text-center">
+		<div id="card-display-left" class="card shadow-sm">
 			<div class="card-body">
 				<div class="row justify-content-center g-2">
-					<div class="col-sm">
+					<div class="col-sm-3">
 						<div class="card-body">
-							test
+							<div class="logo-container">
+								<img src="./images/mavericks-logo.png" alt="mavericks-logo">
+							</div>
+							Mavericks
+							<br>
+							48-29
 						</div>
 					</div>
-					<div class="col-sm">
-						<div class="card-body">
-							test
+					<div class="col-2  pt-3">
+						<div class="  final-score">
+							120
 						</div>
 					</div>
-					<div class="col-sm">
-						<div class="card-body">
-							test
+					<div class="col  pt-4">
+						<div class="">
+							FINAL
 						</div>
 					</div>
-					<div class="col-sm">
-						<div class="card-body">
-							test
+					<div class="col-2  pt-3">
+						<div class=" final-score">
+							112
 						</div>
 					</div>
-					<div class="col-sm">
+					<div class="col-sm-3">
 						<div class="card-body">
-							test
+							<div class="logo-container">
+								<img src="./images/cavaliers-logo.png" alt="cavaliers-logo">
+							</div>
+							Cavaliers
+							<br>
+							48-29
 						</div>
 					</div>
 				</div>
@@ -36,17 +46,17 @@
 				<div class="row g-0">
 					<div class="col-sm">
 						<div class="foot-btn">
-							test
+							04/05/2022
 						</div>
 					</div>
 					<div class="col-sm">
 						<div class="foot-btn foot-btn-center">
-							test
+							BOX SCORE
 						</div>
 					</div>
 					<div class="col-sm">
 						<div class="foot-btn">
-							test
+							GAME DETAILS
 						</div>
 					</div>
 				</div>
@@ -57,30 +67,71 @@
 	<div class="col-lg">
 		<div class="card shadow-sm">
 			<div class="card-body">
-				<p>Game Leaders</p>
-				<table class="table">
+				<!-- <p class="text-dark fw-bold">Game Leaders</p> -->
+				<table class="table table-responsive-sm table-hover align-middle caption-top">
+					<caption class="fw-bold text-dark">Game Leaders</caption>
 					<thead>
 						<tr>
-							<th scope="col">#</th>
-							<th scope="col">First</th>
-							<th scope="col">Last</th>
-							<th scope="col">Handle</th>
+							<th scope="col">PLAYER</th>
+							<th scope="col">PTS</th>
+							<th scope="col">REB</th>
+							<th scope="col">AST</th>
 						</tr>
 					</thead>
 					<tbody>
+						<!-- PLAYER STATS TEMPLATE -->
 						<tr>
-							<th scope="row">1</th>
-							<td>Mark</td>
-							<td>Otto</td>
-							<td>@mdo</td>
+							<td scope="row">
+								<div class="row g-0">
+									<div class="col-sm-1 me-2">
+										<!-- <div> -->
+										<img class="pic" src="./images/luka-pic.jpg" alt="Luka">
+										<!-- </div> -->
+									</div>
+									<div class="col-sm fw-normal">
+										<div class="player-info">
+											<div class="player-name">
+												Luka Doncic
+											</div>
+											<div class="player-details">
+												DAL | #77 | F-C
+											</div>
+										</div>
+									</div>
+								</div>
+							</td>
+							<td>35</td>
+							<td>9</td>
+							<td>13</td>
 						</tr>
+						<!-- END OF PLAYER STATS TEMPLATE -->
+						<!-- PLAYER STATS TEMPLATE -->
 						<tr>
-							<th scope="row">2</th>
-							<td>Jacob</td>
-							<td>Thornton</td>
-							<td>@fat</td>
+							<td scope="row">
+								<div class="row g-0">
+									<div class="col-sm-1 me-2">
+										<!-- <div> -->
+										<img class="pic" src="./images/luka-pic.jpg" alt="Luka">
+										<!-- </div> -->
+									</div>
+									<div class="col-sm fw-normal">
+										<div class="player-info">
+											<div class="player-name">
+												Luka Doncic
+											</div>
+											<div class="player-details">
+												DAL | #77 | F-C
+											</div>
+										</div>
+									</div>
+								</div>
+							</td>
+							<td>35</td>
+							<td>9</td>
+							<td>13</td>
 						</tr>
-					</tbody>
+						<!-- END OF PLAYER STATS TEMPLATE -->
+						</tbody>
 				</table>
 			</div>
 		</div>
