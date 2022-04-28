ffba3567743400b3165fe370cef690f0034acc1d
AApalafox <adreanpalafox@gmail.com>
2022-04-29 07:48:40 +0800

    adjusted logs contents

--- pages/logs.php
+++ pages/logs.php
@@ -140,23 +140,41 @@ if (!isset($_COOKIE['isLogin'])) {
 
             <!-- page content here -->
             <div class="content">
-                <div class="row gy-3">
+                <div class="row gx-0 gy-3">
                     <div class="col-lg-4">
                         <div class="border-card">
-                            <h1 class="pb-3">Admin-Logs</h1>
-                            <div class="col-7 pb-3">
-                                <form class="d-flex" action="search.php">
-                                    <input class="form-control" type="search" placeholder="Search Username" aria-label="Search">
-                                    <button class="btn ms-2" type="submit">
-                                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" style=" fill:#fff;">
-                                            <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"></path>
-                                        </svg>
-                                    </button>
-                                </form>
+                            <div class="logs-header">
+                                <h1 class="pb-3">Admin-Logs</h1>
+
+                                <!-- <div class="col-7 pb-3">
+                                    <form class="d-flex" action="search.php">
+                                        <input class="form-control" type="search" placeholder="Search Username" aria-label="Search">
+                                        <button class="btn ms-2" type="submit">
+                                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" style=" fill:#fff;">
+                                                <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"></path>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div> -->
+                                <div for="useranme-search" class="content-search-container mb-3">
+                                    <div class="content-search pt-2 ps-3">
+                                        <label for="username-search">
+                                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" style=" fill:#7380ec;">
+                                                <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"></path>
+                                            </svg>
+                                        </label>
+                                    </div>
+                                    <div class="content-search">
+                                        <form class="d-flex" action="search.php">
+                                            <input id="username-search" class="form-control" type="search" placeholder="Search Username" aria-label="Search">
+                                        </form>
+                                    </div>
+                                </div>
+
+                                <button class="logs-btn-tab selected">Admin</button>
+                                <button class="logs-btn-tab">Exploit</button>
+                                <button class="logs-btn-tab">Purchase</button>
                             </div>
-                            <button class="logs-btn-tab selected">Admin</button>
-                            <button class="logs-btn-tab">Exploit</button>
-                            <button class="logs-btn-tab">Purchase</button>
                             <div class="logs-card-container">
                                 <!-- END OF CARDS TEMPLATE -->
                                 <div class="template d-none">
@@ -185,19 +203,21 @@ if (!isset($_COOKIE['isLogin'])) {
                     </div>
                     <div id="col-moderator" class="col">
                         <div class="border-card">
-                            <h1 class="pb-3">Staff Moderator</h1>
+                            <div class="logs-header">
+                                <h1 class="pb-3">Staff Moderator</h1>
+                            </div>
                             <div class="logs-card-container">
                                 <!-- END OF CARDS TEMPLATE -->
                                 <div class="template d-none">
-                                    <div id="card-template-mod" class="logs-card p-2">
+                                    <div id="card-template-mod" class="logs-card ps-5 p-2">
                                         <div class="row">
                                             <div class="col-auto">
                                                 <img src="images/zenovain.jpg" class="card-pic" alt="player-pic">
                                             </div>
                                             <div class="col">
-                                                <h3 class="fw-bold">OnlyTwentyCharacters</h3>
-                                                <h3 class="defeat fw-bold">Banned: 1d</h3>
-                                                <h3>Reason: Toxic Activity</h3>
+                                                <h3 class="fw-bold">WhiteHairs</h3>
+                                                <h4 class="defeat fw-bold">Banned: 1d</h4>
+                                                <h4>Reason: Toxic Activity</h4>
                                             </div>
                                         </div>
                                     </div>
