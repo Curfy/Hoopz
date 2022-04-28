c2d549aaf4e355443a330d1516634e6bdcdfe4b1
AApalafox <adreanpalafox@gmail.com>
2022-04-29 00:44:39 +0800

    added logs

---
+++ pages/logs.js
@@ -0,0 +1,57 @@
+currLogId = 1;
+currModId = 1;
+var myTemplateLog = $('#card-template-log')[0].outerHTML;
+var myTemplateMod = $('#card-template-mod')[0].outerHTML;
+
+function loadLog(){
+	let stringId = currLogId.toString();
+	// add the template before the benchmark
+	$("#toStackLog").before(myTemplateLog);
+	// change the id of the newly added template
+	$("#toStackLog").prev().attr("id", stringId);
+
+
+
+
+	// increment for next template
+	currLogId += 1;
+}
+
+function loadMod(){
+	let stringId = currModId.toString();
+	// add the template before the benchmark
+	$("#toStackMod").before(myTemplateMod);
+	// change the id of the newly added template
+	$("#toStackMod").prev().attr("id", stringId);
+
+
+
+
+	// increment for next template
+	currModId += 1;
+}
+
+for(let i=0; i<10; i++){
+	loadLog();
+}
+for(let i=0; i<7; i++){
+	loadMod();
+}
+
+// FOR #col-moderator order on resize
+if ($(window).width() < 992){
+	$("#col-moderator").addClass("order-first");
+}else{
+	if ($("#col-moderator").hasClass("order-first")){
+		$("#col-moderator").removeClass("order-first");
+	}
+}
+$( window ).resize(function() {
+  if ($(window).width() < 992){
+		$("#col-moderator").addClass("order-first");
+	}else{
+		if ($("#col-moderator").hasClass("order-first")){
+			$("#col-moderator").removeClass("order-first");
+		}
+	}
+});
