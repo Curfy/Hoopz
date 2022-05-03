36174099e37075db414625a1ab2e99993ed1c267
AApalafox <adreanpalafox@gmail.com>
2022-05-03 20:21:47 +0800

    separated header file

--- pages/matches.php
+++ pages/matches.php
@@ -7,153 +7,205 @@ if (!isset($_COOKIE['isLogin'])) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <title>Hoopz</title>
+<?php include 'header.php'; ?>
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- MATERIAL ICONS -->
-    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
-
-    <!-- CUSTOM STYLESHEET -->
-    <link rel="stylesheet" href="./style.css">
-
-    <!-- JQUERY JAVASCRIPT -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-    <!-- For Calendar Module -->
-    <link href="https://cdn.jsdelivr.net/npm/bulma-calendar@6.0.7/dist/css/bulma-calendar.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bulma-calendar@6.1.18/dist/js/bulma-calendar.min.js"></script>
-
-</head>
-
-<body>
-    <div class="mycontainer">
-        <aside>
-            <div class="top">
-                <div class="logo">
-                    <img src="./images/hoopzlogo.png" alt="">
-                    <h2 class="danger" style="font-weight: bold">HO<span class="score">OPZ</span></h2>
-                </div>
-                <div class="close" id="close-btn">
-                    <span class="material-icons-sharp">close</span>
-                </div>
-            </div>
-
-            <div class="sidebar">
-
-                <a>
-                    <span>
-                        <label for="player-search" class="search-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="30" viewBox="0 0 30 30" style=" fill:#677483;">
-                                <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+<!-- page content here -->
+<div class="content">
+    <div class="header">
+        <div class="row mb-5 justify-content-between">
+            <div class="col-auto">
+                <div class="content-search-container mb-3">
+                    <div class="content-search pt-2 ps-3">
+                        <label for="team-search">
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" style=" fill:#7380ec;">
+                                <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"></path>
                             </svg>
                         </label>
-                        <div class="sidenav-search-container mb-3">
-                            <div class="content-search">
-                                <label for="player-search">
-                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" style=" fill:#677483;">
-                                        <path d="M 9 2 C 5.1458514 2 2 5.1458514 2 9 C 2 12.854149 5.1458514 16 9 16 C 10.747998 16 12.345009 15.348024 13.574219 14.28125 L 14 14.707031 L 14 16 L 20 22 L 22 20 L 16 14 L 14.707031 14 L 14.28125 13.574219 C 15.348024 12.345009 16 10.747998 16 9 C 16 5.1458514 12.854149 2 9 2 z M 9 4 C 11.773268 4 14 6.2267316 14 9 C 14 11.773268 11.773268 14 9 14 C 6.2267316 14 4 11.773268 4 9 C 4 6.2267316 6.2267316 4 9 4 z"></path>
-                                    </svg>
-                                </label>
-                            </div>
-                            <div class="content-search">
-                                <form class="d-flex" action="search.php">
-                                    <input id="player-search" class="form-control" type="search" placeholder="Search Player" aria-label="Search">
-                                </form>
-                            </div>
-                        </div>
-                    </span>
-                </a>
-
-                <a href="dashboard.php">
-                    <span class="material-icons-sharp">grid_view</span>
-                    <h3>Dashboard</h3>
-                </a>
-
-                <a href="matches.php" class="active">
-                    <span class="material-icons-outlined">sports_esports</span>
-                    <h3>Matches</h3>
-                </a>
-
-                <a href="#">
-                    <span class="material-icons-outlined">shopping_bag</span>
-                    <h3>Purchases</h3>
-                </a>
-
-                <a href="logs.php">
-                    <span class="material-icons-outlined">book</span>
-                    <h3>Logs</h3>
-                </a>
-
-                <a href="#">
-                    <span class="material-icons-outlined">mail_outline</span>
-                    <h3>Messages</h3>
-                    <span class="message-count">26</span>
-                </a>
-
-                <a href="#">
-                    <span class="material-icons-outlined">trending_up</span>
-                    <h3>Analytics</h3>
-                </a>
-
-                <a href="#">
-                    <span class="material-icons-sharp">settings</span>
-                    <h3>Settings</h3>
-                </a>
-
-                <a id="logoutBtn" class="logout">
-                    <span class="material-icons-outlined">logout</span>
-                    <h3>Logout</h3>
-                </a>
-            </div>
-        </aside>
-
-        <!-- END OF ASIDE -->
-
-        <main id="main-body">
-            <div class="right">
-                <h1 class="ps-5">Matches</h1>
-                <!-- <div class="text">
-                    <input type="text" placeholder="Search Player">
-                </div> -->
-                <div class="top">
-                    <button id="menu-btn">
-                        <span class="material-icons-round">menu</span>
-                    </button>
-                    <div class="theme-toggler">
-                        <span class="material-icons-round">light_mode</span>
-                        <span class="material-icons-round">dark_mode</span>
                     </div>
+                    <div class="content-search">
+                        <form class="d-flex" action="search.php">
+                            <input id="team-search" class="form-control" type="search" placeholder="Search Team" aria-label="Search">
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto">
+                <div class="date-container">
+                    <div type="date"></div>
+                    <!-- <div placeholder="" readonly="readonly" class="" type="date"></div> -->
+                </div>
+            </div>
+        </div>
+    </div>
 
-                    <div class="profile">
-                        <div class="info">
-                            <p>Welcome, <b>Zenovain</b></p>
-                            <small class="text-muted"><b class="success">Head of Staff</b></small>
+    <!-- CARD DISPLAY TEMPLATE -->
+    <div id="template" class="d-none">
+        <div id="card-template" class="row gx-1 mb-4">
+            <!-- LEFT -->
+            <div class="col-xl-5 text-center">
+                <div id="card-display-left" class="card shadow-sm">
+                    <div class="card-body">
+                        <div class="row justify-content-center g-2">
+                            <div class="col-3">
+                                <div>
+                                    <div class="logo-container">
+                                        <img src="./images/mavericks-logo.png" class="logo" alt="mavericks-logo">
+                                    </div>
+                                    <label id="team-1-name">Mavericks</label>
+                                    <br>
+                                    <label class="team-1-prescore">48-29</label>
+                                </div>
+                            </div>
+                            <div class="col-2  pt-5">
+                                <div class="final-score">
+                                    <p id="team-1-finalscore" class="text-decoration-underline">120</p>
+                                </div>
+                            </div>
+                            <div class="col pt-5">
+                                <p class="pt-3  align-middle">
+                                    FINAL
+                                </p>
+                            </div>
+                            <div class="col-2  pt-5">
+                                <div class="final-score">
+                                    <p id="team-2-finalscore">112</p>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div>
+                                    <div class="logo-container">
+                                        <img src="./images/cavaliers-logo.png" class="logo" alt="cavaliers-logo">
+                                    </div>
+                                    <label id="team-2-name">Cavaliers</label>
+                                    <br>
+                                    <label id="team-2-prescore">48-29</label>
+                                </div>
+                            </div>
                         </div>
-                        <div class="profile-photo">
-                            <img src="./images/zenovain.jpg" alt="">
+                    </div>
+                    <div class="card">
+                        <div class="row g-0 justify-content-between">
+                            <div class="col">
+                                <div class="foot-btn">
+                                    <label id="match-date" class="align-middle">04/05/2022</label>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="foot-btn">
+                                    <a href="#" class="align-middle">
+                                        GAME DETAILS
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <!-- page content here -->
-            <div class="content">
-                <?php include 'matches_data.php' ?>
+            <!-- RIGHT -->
+            <div class="col-lg">
+                <div id="card-display-right" class="card shadow-sm">
+                    <div class="card-body">
+                        <table class="table table-responsive-sm align-middle caption-top">
+                            <caption>Game Leaders</caption>
+                            <thead>
+                                <tr>
+                                    <th scope="col">PLAYER</th>
+                                    <th scope="col">PTS</th>
+                                    <th scope="col">REB</th>
+                                    <th scope="col">AST</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- player-1 -->
+                                <tr>
+                                    <td scope="row">
+                                        <div class="row g-0">
+                                            <div class="col-auto me-2">
+                                                <img id="player-1-pic" class="pic" src="./images/luka-pic.jpg" alt="Luka">
+                                            </div>
+                                            <div class="col fw-normal">
+                                                <div class="player-info">
+                                                    <div class="player-name">
+                                                        <label id="player-1-name">Luka Doncic</label>
+                                                    </div>
+                                                    <div class="player-details">
+                                                        <label id="player-1-details">DAL | #77 | F-C</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td id="player-1-pts">35</td>
+                                    <td id="player-1-reb">9</td>
+                                    <td id="player-1-ast">13</td>
+                                </tr>
+                                <!-- player-2 -->
+                                <tr>
+                                    <td scope="row">
+                                        <div class="row g-0">
+                                            <div class="col-auto me-2">
+                                                <img id="player-2-pic" class="pic" src="./images/luka-pic.jpg" alt="Luka">
+                                            </div>
+                                            <div class="col fw-normal">
+                                                <div class="player-info">
+                                                    <div class="player-name">
+                                                        <label id="player-2-name">Luka Doncic</label>
+                                                    </div>
+                                                    <div class="player-details">
+                                                        <label id="player-2-details">DAL | #77 | F-C</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td id="player-2-pts">35</td>
+                                    <td id="player-2-reb">9</td>
+                                    <td id="player-2-ast">13</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-
-        </main>
-        <!-- END OF MAIN -->
+        </div>
     </div>
+    <!-- END CARD DISPLAY TEMPLATE-->
 
-    <script src="./index.js"></script>
-    <script src="../js/logout.js"></script>
+    <!-- benchmark to stack templates -->
+    <div id="toStack"></div>
+
+    <nav>
+        <ul class="page-group">
+            <a href="#">
+                <li class="group-item">
+                    < </li>
+            </a>
+            <a href="#">
+                <li class="group-item selected">1</li>
+            </a>
+            <a href="#">
+                <li class="group-item">2</li>
+            </a>
+            <a href="#">
+                <li class="group-item">3</li>
+            </a>
+            <a href="#">
+                <li class="group-item"> > </li>
+            </a>
+        </ul>
+    </nav>
+
+    <div class="m-5"><br>
+    </div>
+</div>
+
+</main>
+<!-- END OF MAIN -->
+</div>
+
+<script src="./index.js"></script>
+<script src="../js/logout.js"></script>
+<script src="./matches.js"></script>
 </body>
 
 </html>
